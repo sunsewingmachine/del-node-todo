93752faf22a36266a49346c312cd2caeb568196c
Farook <sunsewingmachine@gmail.com>
2024-08-06 09:59:57 +0530

    new changes

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ app.post('/todos', (req, res) => {
     description
   }
 
-
-  // push changes 
+  // push changes - Good
   todos.push(newTodo);
   console.log(todos);
   res.status(201).json(newTodo);
